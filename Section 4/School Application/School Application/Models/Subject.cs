e040fe80353d4573981992db20662e2f52c8f3f5
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-02 12:31:38 +0530

    my commit

---
+++ Section 4/School Application/School Application/Models/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace School_Application.Models
+{
+    [Table("SubTable")]
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        [Required]
+        public string SubjectName { get; set; }
+
+    }
+}
