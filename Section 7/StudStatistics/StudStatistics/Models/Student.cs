0ce796caada69157be943e27dceecbecc52ab074
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-07 15:03:26 +0530

    my commit

---
+++ Section 7/StudStatistics/StudStatistics/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace StudStatistics.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string StudentName { get; set; }
+        public int TotalMarks { get; set; }
+    }
+}
---
+++ Section 7/StudStatistics/StudStatistics/Models/StudentStatistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudStatistics.Models
+{
+    [Table("StudStatistics")]
+    public class StudentStatistics
+    {
+        [Key]
+        public int StudentCount { get; set; }
+        public Student MaxMarkStudent { get; set; }
+        public Student MinMarkStudent { get; set; }
+        public List<Student> StudentsWithLessThan50Marks { get; set; }
+        public double AverageMark { get; set; }
+    }
+}
